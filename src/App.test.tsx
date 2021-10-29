35f39c2e37aae677a142f1e9fdc30d7d640e4d41
Karol Grydz <karol.grydz@fastwhitecat.com>
2021-10-29 10:13:11 +0200

    added .gif

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,13 @@ import App from "./App";
 
 test("renders title users list", () => {
   render(<App />);
-  const linkElement = waitFor(() => screen.getByText(/users list/i));
+  const linkElement = screen.getByText(/users list/i);
+  // const inputElement = screen.getAllByPlaceholderText(
+  //   /Search by user name.../i
+  // );
+
+  screen.debug();
+
   expect(linkElement).toBeInTheDocument();
+  // expect(inputElement).toBeInTheDocument();
 });
